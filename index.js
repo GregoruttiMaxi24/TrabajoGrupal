54bf471f33a971dc28d72afe23a879cbad754b28
Ivan javier <ig6119838@gmail.com>
2025-06-04 00:31:12 -0300

    update products

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
 const button = document.querySelectorAll('.boton');
+const images = document.querySelectorAll('.container-producto img');
 
 // Recorre los productos 
 button.forEach(card => {
     card.addEventListener('click', () => {
         const producto = card.parentElement.parentElement;
         const titulo = producto.querySelector('.producto-titulo p').textContent
-        const alert = document.querySelector('.alert');
-        const alertText = document.querySelector('.alert p');
-        alertText.textContent = `Se agrego ${titulo}`
-        alert.classList.add('show');
-        setTimeout(() => {
-            alert.classList.remove('show')
-        }, 4000)
+        notification(titulo)
     })
 })
+
+// Efecto zoom
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        const url = img.src
+        const imageUrl = url.slice(22);
+        zoomImage(imageUrl)
+    })
+})
+
+// Notifica al usuario 
+function notification(messege){
+    const alert = document.querySelector('.alert');
+    const alertText = document.querySelector('.alert p');
+    alertText.textContent = `Se agrego ${messege}`
+    alert.classList.add('show');
+    setTimeout(() => {
+        alert.classList.remove('show')
+    }, 4000)
+}
+
+
+function zoomImage(img){
+
+    clearImages();
+    const container = document.querySelector('.catalogo');
+    const containerImage = document.createElement('div');
+    const body = document.querySelector('body');
+    containerImage.classList.add('zoom');
+    containerImage.innerHTML = `
+        <img src='${img}' alt='producto'>
+    `
+    container.appendChild(containerImage);
+
+    containerImage.addEventListener('mouseout', ()=> {
+        container.addEventListener('click', () => {
+            containerImage.remove();
+        })
+    })
+}
+
+// Eliminar imagenes repetidas
+function clearImages(){
+    const img = document.querySelector('.zoom');
+    if(img) return img.remove()
+    
+}
+
+
+
